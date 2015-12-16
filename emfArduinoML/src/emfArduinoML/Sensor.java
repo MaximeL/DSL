6d03922460b1f4cd89fc224e39f6d75fd67fd9d0
Marc Destefanis <marc.destefanis.etu@gmail.com>
2015-12-16 11:54:06 +0100

    run as MWE2 Workflow doesn't work

---
+++ emfArduinoML/src/emfArduinoML/Sensor.java
@@ -0,0 +1,17 @@
+/**
+ */
+package emfArduinoML;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Sensor</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see emfArduinoML.EmfArduinoMLPackage#getSensor()
+ * @model
+ * @generated
+ */
+public interface Sensor extends Brick {
+} // Sensor
---
+++ emfArduinoML/src/emfArduinoML/impl/BrickImpl.java
@@ -0,0 +1,217 @@
+/**
+ */
+package emfArduinoML.impl;
+
+import emfArduinoML.Brick;
+import emfArduinoML.EmfArduinoMLPackage;
+
+import org.eclipse.emf.common.notify.Notification;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Brick</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * </p>
+ * <ul>
+ *   <li>{@link emfArduinoML.impl.BrickImpl#getName <em>Name</em>}</li>
+ *   <li>{@link emfArduinoML.impl.BrickImpl#getPin <em>Pin</em>}</li>
+ * </ul>
+ *
+ * @generated
+ */
+public abstract class BrickImpl extends MinimalEObjectImpl.Container implements Brick {
+	/**
+	 * The default value of the '{@link #getName() <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getName()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final String NAME_EDEFAULT = null;
+
+	/**
+	 * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getName()
+	 * @generated
+	 * @ordered
+	 */
+	protected String name = NAME_EDEFAULT;
+
+	/**
+	 * The default value of the '{@link #getPin() <em>Pin</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getPin()
+	 * @generated
+	 * @ordered
+	 */
+	protected static final int PIN_EDEFAULT = 0;
+
+	/**
+	 * The cached value of the '{@link #getPin() <em>Pin</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see #getPin()
+	 * @generated
+	 * @ordered
+	 */
+	protected int pin = PIN_EDEFAULT;
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected BrickImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return EmfArduinoMLPackage.Literals.BRICK;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setName(String newName) {
+		String oldName = name;
+		name = newName;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, EmfArduinoMLPackage.BRICK__NAME, oldName, name));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public int getPin() {
+		return pin;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public void setPin(int newPin) {
+		int oldPin = pin;
+		pin = newPin;
+		if (eNotificationRequired())
+			eNotify(new ENotificationImpl(this, Notification.SET, EmfArduinoMLPackage.BRICK__PIN, oldPin, pin));
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public Object eGet(int featureID, boolean resolve, boolean coreType) {
+		switch (featureID) {
+			case EmfArduinoMLPackage.BRICK__NAME:
+				return getName();
+			case EmfArduinoMLPackage.BRICK__PIN:
+				return getPin();
+		}
+		return super.eGet(featureID, resolve, coreType);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eSet(int featureID, Object newValue) {
+		switch (featureID) {
+			case EmfArduinoMLPackage.BRICK__NAME:
+				setName((String)newValue);
+				return;
+			case EmfArduinoMLPackage.BRICK__PIN:
+				setPin((Integer)newValue);
+				return;
+		}
+		super.eSet(featureID, newValue);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public void eUnset(int featureID) {
+		switch (featureID) {
+			case EmfArduinoMLPackage.BRICK__NAME:
+				setName(NAME_EDEFAULT);
+				return;
+			case EmfArduinoMLPackage.BRICK__PIN:
+				setPin(PIN_EDEFAULT);
+				return;
+		}
+		super.eUnset(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean eIsSet(int featureID) {
+		switch (featureID) {
+			case EmfArduinoMLPackage.BRICK__NAME:
+				return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+			case EmfArduinoMLPackage.BRICK__PIN:
+				return pin != PIN_EDEFAULT;
+		}
+		return super.eIsSet(featureID);
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public String toString() {
+		if (eIsProxy()) return super.toString();
+
+		StringBuffer result = new StringBuffer(super.toString());
+		result.append(" (name: ");
+		result.append(name);
+		result.append(", pin: ");
+		result.append(pin);
+		result.append(')');
+		return result.toString();
+	}
+
+} //BrickImpl
